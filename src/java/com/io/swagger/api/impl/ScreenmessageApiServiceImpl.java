8beb7e70d120f7fe494a3f0e39bb589554798c5b
gomul <alface@gomuls.nyc.rr.com>
2018-01-24 22:41:47 -0500

    fix warning

--- src/java/com/io/swagger/api/impl/ScreenmessageApiServiceImpl.java
+++ src/java/com/io/swagger/api/impl/ScreenmessageApiServiceImpl.java
@@ -3,14 +3,9 @@ package com.io.swagger.api.impl;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.io.swagger.api.ScreenmessageApi;
 
 public class ScreenmessageApiServiceImpl implements ScreenmessageApi {
-
-	private static final Logger LOG = LoggerFactory.getLogger(ScreenmessageApiServiceImpl.class.getSimpleName());
 
 	public Response screenmessage(String body) {
 		Response response = Response.ok().type(MediaType.APPLICATION_XML).build();
